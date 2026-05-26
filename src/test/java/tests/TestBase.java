bc186af857ac26bb4cb1c97040d8a961fd75d95e
Tatiana Svetlitskaia <tanyasvetlitskaya@gmail.com>
2026-05-26 23:42:20 +0300

    Fixes after review: TestBase and build.gradle refactoring

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,26 +1,27 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
 
 import java.util.Map;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
 
 public class TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
 
-//    @BeforeAll
-//    static void setupSelenideEnv() {
-//        Configuration.browser = "chrome";
-//        Configuration.browserSize = "1920x1080";
-//        Configuration.baseUrl = "https://demoqa.com";
-//        Configuration.timeout = 10000; // default 4000
-//    }
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
 
     @BeforeAll
     static void beforeAll() {
@@ -28,7 +29,6 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.browser = "chrome";
         Configuration.browserVersion = "128.0";
-//        Configuration.browserVersion = "130.0";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -43,15 +43,11 @@ public class TestBase {
 
 
     @AfterEach
-    void addAttachments() {
+    void afterEach() {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
-    }
-
-    //        Attach.attachAsText("Some file", "Some content");
-    void closeWebDriver() {
-        Selenide.closeWebDriver();
+        closeWebDriver();
     }
 }
